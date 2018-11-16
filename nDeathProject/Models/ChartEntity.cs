aa07c4486414ace22e1b9b54708a9e38b80370fb
Tsar Alexandr <tantal73180@gmail.com>
2018-11-16 19:12:58 +0300

    Add ChartEntity model

---
+++ nDeathProject/Models/ChartEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace nDeathProject.Models
+{
+    public class ChartEntity
+    {
+        public int ACoeff { get; set; }
+        public int BCoeff { get; set; }
+        public int CCoeff { get; set; }
+        public int Step { get; set; }
+        public int LowerBorder { get; set; }
+        public int UpperBorder { get; set; }
+    }
+}
